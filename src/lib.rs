10125550deafda795b7c43fe0cb8e802b80c19f7
DrinoSan <s.becirbegovic@gmail.com>
2023-08-06 12:53:36 +0200

    Restructured unit tests, added api client

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,6 @@
 pub mod configuration;
-
 pub mod domain;
+pub mod email_client;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
-
-// #[get("/health_check")]
-// async fn health_check() -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
-
-// #[derive(Deserialize)]
-// struct FormData {
-//     email: String,
-//     name: String
-// }
-
-// #[post("/subscriptions")]
-// async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
-
-// pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-//     let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
-//         .listen(listener)?
-//         .run();
-
-//     Ok(server)
-// }
